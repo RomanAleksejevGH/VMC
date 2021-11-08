2e4c4a15e40d666573345a3c1531cf778886084e
Roman Aleksejev <skydead@gmail.com>
2021-11-08 19:13:45 +0200

    08.11

--- VMC/modeles/CModel.php
+++ VMC/modeles/CModel.php
@@ -2,23 +2,23 @@
 
 class Cmodel{
     public function GetData(){
-        $arrayResult[] = array(
-            'TITLE'=> 'Blog hosting site',
-            'DATE'=>'22.06.2020',
-            'AUTHOR'=>'VolDeMort',
-            'IMAGE'=>'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
-            'TEXT'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat delectus molestias ipsam dicta omnis reprehenderit, iste alias voluptates atque expedita velit fugit officiis. Quia eum ipsum, repudiandae fugit adipisci ea!'
-            
-        );
+       $host = '127.0.0.1';
+       $db = 'blog';
+       $user = 'root';
+       $pass = '';
+       $charset = 'utf8';
 
-        $arrayResult[] = array(
-            'TITLE'=> 'Blog hosting site',
-            'DATE'=>'21.10.2021',
-            'AUTHOR'=>'Igor',
-            'IMAGE'=>'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
-            'TEXT'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat delectus molestias ipsam dicta omnis reprehenderit, iste alias voluptates atque expedita velit fugit officiis. Quia eum ipsum, repudiandae fugit adipisci ea!'
-            
-        );
+       $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+       $opt = [
+           PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+           PDO::ATTR_EMULATE_PREPARES => false,
+       ];
+
+       $pdo = new PDO($dsn, $user, $pass, $opt);
+
+       $result = $pdo->query('SELECT * From news');
+       $arrayResult = $result->fetchAll();
 
         return $arrayResult;
 
