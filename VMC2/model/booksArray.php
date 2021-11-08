38c18cbb532329a0c443a6f7e1045dafe98c50f8
Roman Aleksejev <skydead@gmail.com>
2021-11-08 20:03:23 +0200

    VMC2

---
+++ VMC2/model/booksArray.php
@@ -0,0 +1,38 @@
+<?php
+$books = array(
+    array(
+        "bookname" => "HTML and CSS",
+        "author" => "Jon Duckett",
+        "price" => "17.19$",
+        "description"=> "description",
+        "image"=>"HTMLandCSS_Duckett.jpg"
+    ),
+    array(
+        "bookname" => "HTML5",
+        "author" => "Matthew MacDonald",
+        "price" => "27.65$",
+        "description"=> "description",
+        "image"=>"HTML5TheMissing_MacDonald.jpg"
+    ),
+    array(
+        "bookname" => "Head First JavaScript Programming",
+        "author" => "Eric Freeman",
+        "price" => "37.60$",
+        "description"=> "description",
+        "image"=>"HeadFirstJavaScript.jpg"
+    ),
+    array(
+        "bookname" => "Head First HTML5 Programming",
+        "author" => "Eric Freeman",
+        "price" => "35.35$",
+        "description"=> "description",
+        "image"=>"HeadFirstHTML5.jpg"
+    ),
+    array(
+        "bookname" => "jQuery in Action",
+        "author" => "Bear Bibeault",
+        "price" => "32.12$",
+        "description"=> "description",
+        "image"=>"JQueryInAction.jpg"
+    ),
+);
